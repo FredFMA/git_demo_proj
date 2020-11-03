9591caabfa9fdfcd7575a0421ab1c9f500bebfa4
fredrik-markussen <fredrik.a.markussen@uit.no>
2020-11-03 13:39:16 +0100

    Add terminal logg

---
+++ Terminal logg explaining stuff.R
@@ -0,0 +1,8 @@
+#How to add a remote repository from github
+#fredrik@P51s:~$ cd ~/git_demo_proj 
+#fredrik@P51s:~/git_demo_proj$ git remote add origin https://github.com/FredFMA/git_demo_proj.git
+#fredrik@P51s:~/git_demo_proj$ git remote
+#origin
+#fredrik@P51s:~/git_demo_proj$ git remote -v
+#origin  https://github.com/FredFMA/git_demo_proj.git (fetch)
+#origin  https://github.com/FredFMA/git_demo_proj.git (push)
